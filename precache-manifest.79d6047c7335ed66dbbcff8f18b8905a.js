0b7ff9152046943126df96568fc21f077505a147
Tobiasz Bielski <tobiaszbielski@wp.pl>
2019-07-11 23:07:53 +0200

    Updates

---
+++ precache-manifest.79d6047c7335ed66dbbcff8f18b8905a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "79ac823e4dd3728eac6d",
+    "url": "/react-app/static/js/main.bdbdf8bb.chunk.js"
+  },
+  {
+    "revision": "5d3b1c8a762b29368c1b",
+    "url": "/react-app/static/js/2.ca2e8aa3.chunk.js"
+  },
+  {
+    "revision": "79ac823e4dd3728eac6d",
+    "url": "/react-app/static/css/main.875522d9.chunk.css"
+  },
+  {
+    "revision": "13d6200da6be2aa38187ae4fd6015da9",
+    "url": "/react-app/index.html"
+  }
+];
